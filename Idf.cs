de43831689b3e27ba2e082e33cecddf543e72e8d
itzchak <s0527120124@gmail.com>
2025-05-21 12:43:00 +0300

    add Idf class

---
+++ Idf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Project
+{
+    internal class Idf
+    {
+        public string DateOfEstablishment = "05/1948";
+        public string Comander = "yoyo";
+        List<StrikeOption> = new List<StrikeOption>();  
+    }
+}
