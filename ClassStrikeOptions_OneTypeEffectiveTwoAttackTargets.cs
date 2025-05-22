50e4629a7b039e987b0cf08e6d5284b6730399ed
Shaul Kimel <shaullk600@gmail.com>
2025-05-22 15:58:07 +0300

    Update File ClassStrikeOptions All

--- ClassStrikeOptions_OneTypeEffectiveTwoAttackTargets.cs
+++ ClassStrikeOptions_OneTypeEffectiveTwoAttackTargets.cs
@@ -8,5 +8,13 @@ namespace IDF_Operatuin
 {
     internal class ClassStrikeOptions_OneTypeEffectiveTwoAttackTargets
     {
+        public string NameOfThetool;//שם הכלי
+
+        public List<string> TypesOfBombs;//סוגי פצצות
+        public Dictionary<string, int> SumOfBombs = new Dictionary<string, int>();//סך הפצצות
+
+        public List<string> EffectiveAgainst;//יעיל נגד
+
+        public string PoweredBy;//מופעל על ידי 
     }
 }
