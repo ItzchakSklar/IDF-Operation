b8119dae9c1bdbef2b4b48e46ac56ad40b33f814
Shaul Kimel <shaullk600@gmail.com>
2025-05-22 15:04:26 +0300

    Create File ClassAbstractStrikeOption.cs this Abstract for Strike Options

---
+++ ClassAbstractStrikeOption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Operatuin
+{
+    internal abstract class ClassAbstractStrikeOption 
+    {
+        //את ההכללה של האינטר פייס צריך לעשולת רק במיין נראה לי
+        public string NameOfThetool;//שם הכלי
+        public List<string> EffectiveAgainst;//יעיל נגד
+        public List<string> TypesOfBombs;//סוגי פצצות
+        public List<string> VulnerableTargets; //הצגת נגד מה הם יעילים בתקיפה
+
+        public abstract void CalculationOfFuelForBombing();//חישוב דלק להפצצה
+        public abstract void ShowUniqueName();
+        public abstract void  ShowTheAmountOfAmmunitionRemaining();
+        public abstract void ShowWhetherAnAttack();
+    }
+}
