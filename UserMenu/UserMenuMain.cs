2a6339c27d08ec802425d31379c5dce24008c71f
Shaul Kimel <shaullk600@gmail.com>
2025-05-29 10:25:05 +0300

    Create new Folder 'User Menu'

---
+++ UserMenu/UserMenuMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IDF_Operatuin;
+
+namespace IDF_Operatuin.UserMenu
+{
+    internal class UserMenuMain
+    {
+        private static UserMenuMain instance = null;
+        private static readonly object lockObj = new object();
+    
+        //constractor
+        private UserMenuMain() { }
+
+        public static UserMenuMain GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockObj) //נעילת האובייקט
+                {
+                    if (instance == null)
+                    {
+                        instance = new UserMenuMain();
+                    }
+                }
+            }
+            return instance;
+        }
+
+
+    }
+}
